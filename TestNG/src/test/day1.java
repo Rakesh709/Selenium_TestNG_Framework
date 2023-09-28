3dbc3f71c5b10c1df6e10c4c1ffe47deaae814ff
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-09-28 11:19:52 +0530

    TestNG Code initial commit

---
+++ TestNG/src/test/day1.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day1 {
+	
+	@Test
+	public void Demo() {
+		System.out.println("Hello World!");
+	}
+	
+	@Test
+	public void SecondDemo() {
+		System.out.println("Bye");
+	}
+
+}
---
+++ TestNG/src/test/day2.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day2 {
+	
+	@Test
+	public void third() {
+		System.out.println("Good");
+	}
+
+}
