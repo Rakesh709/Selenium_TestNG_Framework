17a53ae52ef9e7be1877024c3e7e5573d355e982
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-09-28 12:28:19 +0530

    Additinal TestNg Code added

---
+++ TestNG/src/test/day3.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day3 {
+	
+	@Test
+	public void webLoginHomeLoan() {
+		System.out.println("enter to weblogin");
+	}
+	
+	@Test
+	public void mobileLoginHomeLoan() {
+		System.out.println("enter to mobile login");
+	}
+	
+	@Test
+	public void loginAPIHomeLoan() {
+		System.out.println("enter to login api");
+	}
+}
---
+++ TestNG/src/test/day4.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class day4 {
+	
+	@Test
+	public void webLoginCarLoan() {
+		System.out.println("enter to weblogin");
+	}
+	
+	@Test
+	public void mobileLoginCarLoan() {
+		System.out.println("enter to mobile login");
+	}
+	
+	@Test
+	public void loginAPICarLoan() {
+		System.out.println("enter to login api");
+	}
+}
