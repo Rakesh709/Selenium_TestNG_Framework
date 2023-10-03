33a4055334e25d983b68c24e7344ad242da9640b
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-10-03 13:28:21 +0530

    Helpers Annotations(dependsOnMethods,enable,TimeOut) In TestNG

--- TestNG/src/test/day5.java
+++ TestNG/src/test/day5.java
@@ -2,31 +2,35 @@ package test;
 
 import org.testng.annotations.Test;
 
-public class day3 {
+public class day5 {
 	
-	@Test(groups= {"Smoke"})
+	@Test
 	public void webLoginHomeLoan() {
 		System.out.println("enter to weblogin");
 	}
+
 	
-	@Test
+	@Test(dependsOnMethods= {"webLoginHomeLoan"})
+	public void apiLoginHomeLoan() {
+		System.out.println("enter to login API");
+	}
+	
+	
+	@Test(enabled=false)
 	public void mobileLoginHomeLoan() {
 		System.out.println("enter to mobile login");
 	}
 	
-	@Test
+	
+	@Test(timeOut=4000)
 	public void mobileLoginHomeLoan2() {
-		System.out.println("enter to mobile login");
+		System.out.println("enter to mobile login2");
 	}
 	@Test
 	public void mobileLoginHomeLoan3() {
-		System.out.println("enter to mobile login");
+		System.out.println("enter to mobile login3");
 	}
 	
 	
 	
-	@Test(dependsOnMethods= {"webLoginHomeLoan","mobile.*"})
-	public void apiLoginHomeLoan() {
-		System.out.println("enter to login API");
-	}
 }
