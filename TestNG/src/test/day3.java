7af6a820ee38d126ba0b981e5207e55793d312f0
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-10-05 11:39:30 +0530

    DataProvider Annotation - Parameterizing

--- TestNG/src/test/day3.java
+++ TestNG/src/test/day3.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class day3 {
@@ -18,9 +19,12 @@ public class day3 {
 	public void mobileLoginHomeLoan2() {
 		System.out.println("enter to mobile login");
 	}
+	
+	@Parameters({"URL"})
 	@Test
-	public void mobileLoginHomeLoan3() {
+	public void mobileLoginHomeLoan3(String urlname) {
 		System.out.println("enter to mobile login");
+		System.out.println(urlname);
 	}
 	
 	
--- TestNG/src/test/day5.java
+++ TestNG/src/test/day5.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 
 
@@ -13,17 +14,43 @@ public class day5 {
 		System.out.println("enter to mobile login");
 	}
 	
-	
-	@Test(timeOut=4000)
-	public void mobileLoginHomeLoan2() {
+	@Test(dataProvider="getData")
+//	@Test(timeOut=4000)
+	public void mobileLoginHomeLoan2(String username, String password) {
 		System.out.println("enter to mobile login2");
+		System.out.println(username);
+		System.out.println(password);
 	}
 	
-	@Parameters({"URL"})
+	@Parameters({"KEY"})
 	@Test
-	public void mobileLoginHomeLoan3(String urlname) {
+	public void mobileLoginHomeLoan3(String key) {
 		System.out.println("enter to mobile login3");
-		System.out.println(urlname);
+		
+		System.out.println(key);
+	}
+	
+	@DataProvider
+	public Object[][] getData() {
+		//1st combination -> username & password -> good credit
+		//2nd combination -> username & password -> no credit
+		//3rd combination -> username & password -> frad
+		
+		Object[][] data = new Object[3][2];
+		
+		//1st combination 
+		data[0][0]="firstusername";
+		data[0][1]="firsPassword";
+		
+		//2nd combination
+		data[1][0]="secondUser";
+		data[1][1]="secoundPass";
+		
+		//3rd combination
+		data[2][0]="thirduser";
+		data[2][1]="thirdPass";
+		
+		return data;
 	}
 	
 	
