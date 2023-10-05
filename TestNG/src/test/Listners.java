d4d6afa299dfe842ba3d8c106185d072f5b126ed
Rakesh709 <kumar.rakesh1997@outlook.com>
2023-10-05 12:37:30 +0530

    Listeners interface in TestNG

---
+++ TestNG/src/test/Listners.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.testng.ITestListener;
+
+//import org.testng.ITestListener;
+
+//import ITestListener(org.testng) 
+//
+//import org.testng.ITestListener;
+
+//ITestListner interface which implement testng listners
+
+public class Listners implements ITestListener {
+		// not able to implement the methods automatic so leave it blank as of now.
+}
+ 
